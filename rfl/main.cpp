206b173c85b365b079f909f5262d2e9b6a0b8224
benyuan <benyuan@tencent.com>
2024-09-23 23:01:19 +0800

    v0.1

--- rfl/main.cpp
+++ rfl/main.cpp
@@ -1,17 +1,76 @@
-import rfl;
-import std;
+//inspired by https://github.com/yosh-matsuda/field-reflection
+#include <iostream>
+#include <string_view>
+#include <source_location>
 
-struct A
+struct Test
 {
-    int a = 0;
-    int b = 1;
-    char c[10] = { '\0' };
-    int* d = nullptr;
-    std::vector<int> e;
+    char a[2];
 };
+
+
+template <typename T>
+struct Wrapper
+{
+    const T value;
+    const static Wrapper fake;
+};
+
+
+
+template <typename T>
+constexpr auto MakePtrTuple(T&& t)
+{
+    auto& [f0] = t;
+    return std::tuple(&f0);
+}
+
+template <typename T, std::size_t N>
+consteval auto GetMemPtr() noexcept
+{
+#if defined(__clang__)
+    return wrapper(std::get<N>(to_ptr_tuple(wrapper<T>::fake.value)));
+#else
+    return std::get<N>(MakePtrTuple(Wrapper<T>::fake.value));
+#endif
+}
+
+
+template <typename T, auto Ptr>
+consteval std::string_view GetFunctionName()
+{
+#if defined(__clang__) && defined(_WIN32)
+    return std::string_view{ __PRETTY_FUNCTION__ };
+#else
+    return std::string_view{ std::source_location::current().function_name() };
+#endif
+}
+
+template <typename T, auto Ptr>
+consteval std::string_view GetFieldName()
+{
+    struct FD
+    {
+        void* dummy;
+    };
+    constexpr const std::string_view dummy = "dummy";
+
+    constexpr auto detectorName = GetFunctionName<FD, GetMemPtr<FD, 0>()>();
+    constexpr auto dummyBegin = detectorName.rfind(dummy);
+    constexpr auto suffix = detectorName.substr(dummyBegin + dummy.size());
+    constexpr auto beginSentinel = detectorName[dummyBegin - 1];
+
+    const auto realName = GetFunctionName<T, Ptr>();
+    const auto last = realName.rfind(suffix);
+    const auto begin = realName.rfind(beginSentinel, last - 1) + 1;
+    return realName.substr(begin, last - begin);
+}
+
+
 
 int main()
 {
-    std::cout << rfl::MemberCounter<A>() << std::endl;;
+    
+    std::cout << GetFieldName<Test, GetMemPtr<Test, 0>()>();
     return 0;
 }
--- rfl/rfl.ixx
+++ rfl/rfl.ixx
@@ -1,5 +1,7 @@
 export module rfl;
 
+import std;
+
 export namespace rfl
 {
     struct UniversalType 
@@ -16,4 +18,55 @@ export namespace rfl
         else
             return MemberCounter<T>(Members..., UniversalType{});
     }
+
+    template <class C, std::size_t N>
+    struct ObjTupleView
+    {
+        static consteval auto TupleView() {}
+    };
+
+
+    template <class C>
+    struct ObjTupleView<C, 0>
+    {
+        static consteval auto TupleView() { return std::tie(); }
+    };
+
+    template <class C>
+    struct ObjTupleView<C, 1>
+    {
+        static consteval auto TupleView() 
+        {
+            auto& [e0] = std::declval<std::remove_cvref_t<C>>();
+            auto tup = std::tie(e0);
+            auto get_ptrs = [](auto&... _refs) {
+                return std::make_tuple(&_refs...);
+                };
+            return std::apply(get_ptrs, tup);
+        }
+    };
+
+    template <auto ptr>
+    inline constexpr std::string_view MemberName()
+    {
+        constexpr std::string_view func = __FUNCSIG__;
+        return func;
+    }
+
+    template <class T>
+    struct Wrapper {
+        using Type = T;
+        T v;
+    };
+
+    template <class T>
+    Wrapper(T) -> Wrapper<T>;
+
+    // This workaround is necessary for clang.
+    template <class T>
+    inline constexpr auto wrap(const T& arg) noexcept {
+        return Wrapper{ arg };
+    }
+
+    
 }
